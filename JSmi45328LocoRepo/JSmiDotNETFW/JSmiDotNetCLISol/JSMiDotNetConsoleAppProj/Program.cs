7ce534f82d7bebcbdea1e3151b56a53ce8be146b
asobowale <adelajas.sobowale@gmail.com>
2025-02-11 14:14:41 -0500

    first commit from Local Repo to Remote Repo

---
+++ JSmi45328LocoRepo/JSmiDotNETFW/JSmiDotNetCLISol/JSMiDotNetConsoleAppProj/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("");
+Console.WriteLine("**************************************");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("**************************************");
+Console.WriteLine("");
+
+// Another modification scripts
+Console.WriteLine(""); // creating white space
+Console.WriteLine("What is your name?");
+
+// variable studentName decalation and definition
+var studentName = Console.ReadLine();
+
+Console.WriteLine(""); // creating white space
+// this is the prompt to enter courseNum
+Console.WriteLine("What is this course number?");
+// variable studentName decalation and definition
+var CourseNum = Console.ReadLine();
+
+// Console.WriteLine() to display the content
+Console.WriteLine($"{Environment.NewLine} Hello, {studentName}!");
+Console.WriteLine($"{Environment.NewLine}You are welcome to {CourseNum} \n");
